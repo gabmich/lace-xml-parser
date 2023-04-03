8dc9c3d428d449d69d3a04ce9a581d245a5122dd
gabmichelet <gabmichelet@bluewin.ch>
2023-04-03 08:47:20 +0200

    update

--- LaceParser.py
+++ LaceParser.py
@@ -2,10 +2,11 @@ import os
 import xml.dom.minidom
 from docxtpl import DocxTemplate, InlineImage
 from docx.shared import Mm
-from resources.helpers import get_node_value
+from resources.helpers import get_node_value, create_dt
 
 
-NEEDED_LACE_FILES = ['ImageReport_0.xml',]
+NEEDED_LACE_FILES = ["ImageReport_0.xml",]
+LACE_DT_FORMAT    = "%a, %b %d, %Y %H:%M:%S %Z"
 
 
 class LaceParser():
@@ -21,65 +22,70 @@ class LaceParser():
     def __init__(self, folder_path):
         self.check_folder(folder_path)
         self.parse_xml_document()
-        self.get_all_items()
+        self.extract_all_items()
         self.extract_evidences()
         self.extract_items_content()
 
     def check_folder(self, folder_path):
-        # chef if folder_path exists and ensure that it's a valid directory
+        # checks if folder_path exists and ensure that it's a valid directory
         if os.path.exists(str(folder_path)) and os.path.isdir(str(folder_path)):
-            print(folder_path, 'is a valid folder')
+            print(folder_path, "is a valid folder")
             self.check_presence_of_lace_files(folder_path)
             # self.get_folder_content()
             # self.list_and_rename_files()
         else:
-            print('not a valid folder')
+            print("not a valid folder")
 
     def check_presence_of_lace_files(self, folder_path):
+        # checks if all needed files are in the selected folder
         files_list = os.listdir(folder_path)
 
-        print('Files list of selected folder : ', files_list)
+        print("Files list of selected folder :", files_list)
         if all(f in files_list for f in NEEDED_LACE_FILES):
-            print('ok, all needed files are in the selected folder')
+            print("ok, all needed files are in the selected folder")
             self.folder_path = str(folder_path)
             return
         missing_files = [f for f in NEEDED_LACE_FILES if not f in files_list]
-        print('Following files are missing in the selected folder : ', missing_files)
+        print("Following files are missing in the selected folder :", missing_files)
 
 
     def parse_xml_document(self):
-        self.xml_document = xml.dom.minidom.parse(f'{self.folder_path}/ImageReport_0.xml')
+        self.xml_document = xml.dom.minidom.parse(f"{self.folder_path}/ImageReport_0.xml")
         print(self.xml_document)
 
-    def get_all_items(self):
+    def extract_all_items(self):
         self.xml_items = self.xml_document.getElementsByTagName("Item")
-        print(self.xml_items)
+        print("NOMBRE D'ITEMS TROUVES :", len(self.xml_items))
 
     def extract_evidences(self):
         for item in self.xml_items:
             evidence_id = get_node_value(item, "EvidenceID") # item.getElementsByTagName("EvidenceID")[0].firstChild.nodeValue
             self.evidences_ids.add(evidence_id)
-        self.evidences = [Evidence(evidence_id) for evidence_id in self.evidences_ids]
-        print('EVIDENCES IDS :', self.evidences_ids)
-        print('EVIDENCES OBJECTS :', self.evidences)
+        self.evidences = {evidence_id:[] for evidence_id in self.evidences_ids}
+        print("EVIDENCES IDS :", self.evidences_ids)
+        print("EVIDENCES DICT :", self.evidences)
 
     def extract_items_content(self):
         for evidence in self.evidences:
-            items = []
             for item in self.xml_items:
-                evidence_item = EvidenceItem(
-                                    evidence_id=get_node_value(item,"EvidenceID"),
-                                    file_id=get_node_value(item,"FileID"),
-                                    image_path=get_node_value(item,"Thumbnail"),
-                                    md5=get_node_value(item,"MD5"),
-                                    partition=get_node_value(item,"Partition"),
-                                    full_path=get_node_value(item,"FullPath"),
-                                    file_name=get_node_value(item,"Filename"),
-                                    created_at=get_node_value(item,"CreateDate"))
-                items.append(evidence_item)
+                evidence_id = get_node_value(item,"EvidenceID")
+                if evidence_id == evidence:
+                    created_at = create_dt(get_node_value(item,"CreateDate"), LACE_DT_FORMAT)
+                    updated_at = create_dt(get_node_value(item,"ModifyDate"), LACE_DT_FORMAT)
+                    evidence_item = EvidenceItem(
+                                        evidence_id=evidence_id,
+                                        file_id=get_node_value(item,"FileID"),
+                                        image_path=get_node_value(item,"Thumbnail"),
+                                        md5=get_node_value(item,"MD5"),
+                                        partition=get_node_value(item,"Partition"),
+                                        full_path=get_node_value(item,"FullPath"),
+                                        file_name=get_node_value(item,"Filename"),
+                                        created_at=created_at,
+                                        updated_at=updated_at)
+                    self.evidences[evidence].append(evidence_item)
 
             # ICI : CONVERTIR CREATED_AT EN FORMAT DE DATE PLUS LISIBLE AU BESOIN
-            print('ITEMS :', items)
+            print(f"NOMBRE D'ITEMS CREES POUR EVIDENCE '{evidence}' :", len(self.evidences[evidence]))
             # export = DocxExporter(evidence_items=items, evidence=evidence)
             # print('EXPORT :', export)
 
@@ -102,10 +108,20 @@ class EvidenceItem():
     partition               = ""
     full_path               = ""
     file_name               = ""
-    created_at              = ""
+    created_at              = None
+    update_at               = None
     image                   = None
 
-    def __init__(self,evidence_id,file_id,image_path,md5,partition,full_path,file_name,created_at):
+    def __init__(self,
+                 evidence_id,
+                 file_id,
+                 image_path,
+                 md5,
+                 partition,
+                 full_path,
+                 file_name,
+                 created_at,
+                 updated_at):
         self.evidence_id = evidence_id
         self.file_id = file_id
         self.image_path = image_path
@@ -126,6 +142,7 @@ class EvidenceItem():
             "file_name":self.file_name,
             "image":self.image,
             "created_at":self.created_at,
+            "updated_at":self.updated_at,
             "video_thumbnails_paths":self.video_thumbnails_paths,
         }
 
@@ -142,10 +159,10 @@ class DocxExporter():
     def __init__(self, evidence_items, evidence):
         self.sort_items_by_creation_date(evidence_items)
         self.create_docx_instances()
-        context = { 'items' : self.evidence_items,
-                    'evidence': evidence,
-                    'start_date': self.start_date,
-                    'end_date': self.end_date,}
+        context = { "items" : self.evidence_items,
+                    "evidence": evidence,
+                    "start_date": self.start_date,
+                    "end_date": self.end_date,}
 
         self.listing_docx.render(context)
         self.listing_docx.save(f"Evidence {evidence} list.docx")
@@ -159,7 +176,7 @@ class DocxExporter():
 
     def sort_items_by_creation_date(self, evidence_items):
         # self.evidence_items = sorted(evidence_items, key=lambda item:(item.created_at, item.created_at is None, item.created_at == ""))
-        print('SORTED ITEMS :', self.evidence_items)
+        print("SORTED ITEMS :", self.evidence_items)
 
     def create_docx_img(self):
         image      = InlineImage("",
--- resources/helpers.py
+++ resources/helpers.py
@@ -1,6 +1,27 @@
+from datetime import datetime
+
+
 def get_node_value(item, tag_name):
     if all([item.getElementsByTagName(tag_name),
             item.getElementsByTagName(tag_name)[0],
             item.getElementsByTagName(tag_name)[0].firstChild,]):
         return item.getElementsByTagName(tag_name)[0].firstChild.nodeValue if item.getElementsByTagName(tag_name)[0].firstChild else None
     return
+
+
+def to_chunks(xs, n):
+    """ Split a list into list of chunks of length of n """
+    n = max(1, n)
+    return (xs[i:i+n] for i in range(0, len(xs), n))
+
+
+def create_dt(datetime_string, datetime_format):
+    dt = None 
+
+    if datetime_string:
+        try:
+            dt = datetime.strptime(datetime_string, datetime_format)
+        except Exception as e:
+            print("ERROR WITH DATETIME STRING", datetime_string, "===>", str(e))
+
+    return dt
